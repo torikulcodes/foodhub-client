1b70ee7c2be073199feb495bcf798157e1a1e5c6
md torikul <mdtorikulislamprogrammer@gmail.com>
2026-04-27 15:11:43 +0600

    change backend url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://foodhub-client-eta.vercel.app"+"/api/auth",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL!,
   fetchOptions: {
     credentials: "include",
   },
